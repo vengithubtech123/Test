aa1a2e968287009c4dd6c867cd9976c4e6be322a
knaoh98 <knaoh982002@yahoo.com>
2025-06-30 16:24:20 +0530

    changes to code

--- src/DupString.java
+++ src/DupString.java
@@ -2,6 +2,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.Map.Entry.comparingByKey;
+
 public class DupString {
 
     public  static void main(String args[]) {
@@ -17,5 +19,16 @@ public class DupString {
 
         System.out.println(ss);
        // String stream = Stream.of(line).filter(x->).forEach();
+
+        Map<Integer, String> map = new HashMap<>();
+        map.put(10,"ten");
+        map.put(1,"one");
+        map.put(5,"five");
+        map.put(3,"thre");
+        map.put(4,"four");
+        System.out.println( "Unsorted  by value" + map);
+        map.entrySet().stream().sorted(Map.Entry.<Integer, String>comparingByKey()).forEach(System.out::println);
+
+
     }
 }
